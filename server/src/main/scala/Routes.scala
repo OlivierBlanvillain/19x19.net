b11fcd4563e6c126203525e54507b20697278ca1
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2016-06-05 17:25:46 +0200

    Load accets from disk in dev, from ressources in prod

--- server/src/main/scala/Routes.scala
+++ server/src/main/scala/Routes.scala
@@ -1,10 +1,16 @@
 package server
 
 import akka.http.scaladsl.server.{Directives, Route}
-
+import java.io.File
 object Routes extends Directives {
   def apply(): Route =
-    pathSingleSlash(getFromResource("content/index.html")) ~
-    path(Segment)(s => getFromResource(s"content/$s")) ~
-    path("target" / Segment)(s => getFromResource(s"content/target/$s"))
+    pathSingleSlash(getFrom("content/index.html")) ~
+    path(Segment)(s => getFrom(s"content/$s")) ~
+    path("target" / Segment)(s => getFrom(s"content/target/$s"))
+
+  def getFrom(content: String): Route =
+    if (new File("../LICENSE").exists)
+      getFromFile(s"../static/$content") // From sbt
+    else
+      getFromResource(content) // From a jar
 }
